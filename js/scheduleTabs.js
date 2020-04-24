b84383264708dc479f7f166033e999db17fb5968
Lena <elenanikolaevna999@yandex.ru>
2020-04-24 13:07:30 +0300

    изменение цыета ячеек таблицы при переходе по вкладкам расписания

--- js/scheduleTabs.js
+++ js/scheduleTabs.js
@@ -10,4 +10,39 @@ function checkTabs(elem){
         if (buttons[i].id != elem.id ) buttons[i].setAttribute("class", "input") ;
         // console.log("i=" + i + " id=" + buttons[i].getAttribute('id'));
     }
+
+    var newClass;
+    switch(elem.id){
+        case "0":
+            newClass = " red";
+            break;
+        case "1":
+            newClass = " blue";
+            break;
+        case "2":
+            newClass = " yellow";
+            break;
+        case "3":
+            newClass = " green";
+            break;
+    }
+
+    console.log("new Class=" + newClass);
+
+    var cells = document.getElementsByClassName('cell');
+    var nameClassCell;
+    for (j=0; j<cells.length; j++){
+    
+        nameClassCell = cells[j].getAttribute("class");
+        console.log("nameClassCell=" + nameClassCell);
+
+        if (nameClassCell.indexOf('entry') < 0){
+            nameClassCell = "cell " + newClass;
+        }else{
+            nameClassCell = "cell entry " + newClass;
+        }
+        
+        console.log("new nameClassCell=" + nameClassCell);
+        cells[j].setAttribute("class", nameClassCell) ;
+    }
 }
