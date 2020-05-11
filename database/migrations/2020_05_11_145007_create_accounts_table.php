03e1f3389825ba28c2c0351415819e196fe124ac
Lena <elenanikolaevna999@yandex.ru>
2020-05-11 18:03:11 +0300

    настройка доступа к БД (файл .env) и создание первой таблицы

---
+++ database/migrations/2020_05_11_145007_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Старт миграций.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email');
+            $table->float('weight');
+            $table->float('height');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Отмена действий.
+     *
+     * @return void
+     */
+    public function down(){
+        Schema::dropIfExists('accounts');
+    }
+}
