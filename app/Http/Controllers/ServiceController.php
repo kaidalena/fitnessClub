f8d9c486c10cd74be39bea307e62e308caf989a5
RafaelRaf123 <naruto-saske2010@mail.ru>
2020-06-09 12:42:44 +0300

    Merge branch 'master' of https://github.com/kaidalena/fitnessClub

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -10,21 +10,49 @@ class ServiceController extends Controller{
   public function index(){
     return view('service')->with([
       'services' =>  service::get(),
-      'linksOnTable' => [route('admin.service.forTable')]
+      'linksOnTable' => [route('admin.service.forTable')],
+      'routes' => [
+        "get" => route('admin.news.forTable'),
+        "change" => route('admin.service.change'),
+        'create' => route('admin.service.create'),
+        'delete' => route('admin.service.delete')
+
+      ]
     ]);
   }
 
   public function serviceForTable() {
       $data['titles'] = ["Путь к фото", "Заголовок"];
-      $data['data'] = [];
-
-      foreach (service::select('title', 'img_path')->get() as $service) {
-          $data['data'][] = [
-            'img_path' => $service->img_path,
-            'title' => $service->title,
-          ];
-      }
+      $data['data'] = service::select('img_path', 'title', 'id')->get();
 
       return response()->json($data);
   }
+  public function create(Request $request) {
+    $news = service::create($request->all());
+
+    $news->save();
+
+    return response()->json([
+      'news' => $news
+    ]);
+  }
+
+  public function change(Request $request) {
+    $news = service::whereId($request->id)->first();
+
+    $news->fill($request->except('id'));
+
+    $news->update();
+
+    return response()->json([
+      'news' => $news
+    ]);
+  }
+
+    public function delete(Request $request) {
+      $news = service::whereId($request->id)->first();
+      $news->delete();
+
+      return response()->json();
+    }
   }
